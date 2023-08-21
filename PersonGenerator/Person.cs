d5a40be3cedf7021bb517535bcce61850f37d255
Vasily7628 <mineok44@gmail.com>
2023-08-21 23:31:59 +0700

    Init

---
+++ PersonGenerator/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace PersonGenerator;
+
+public class Person
+{
+    public string[] CreditCardNumbers { get; init; }
+    public int Age { get; init; }
+    public string[] Phones { get; set; }
+    public long BirthDate { get; set; }
+    public double Salary { get; set; }
+    public bool IsMarried { get; set; }
+    public Gender Gender { get; set; }
+    public Child[] Children { get; set; }
+}
+
+public class Child
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public long BirthDate { get; set; }
+    public Gender Gender { get; set; }
+}
+
+public enum Gender
+{
+    Male,
+    Female
+}
---
+++ PersonGenerator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using Bogus;
+
+namespace PersonGenerator;
+
+public static class Program
+{
+    public static void Main()
+    {
+        List<Person> persons = GenerateRandomPersons(10000);
+        string json = JsonSerializer.Serialize(persons,
+            new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+        string programDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string filePath = Path.Combine(programDirectory, "Persons.json");
+        File.WriteAllText(filePath, json);
+        persons.Clear();
+    }
+
+
+    static List<Person> GenerateRandomPersons(int count)
+    {
+        var faker = new Faker();
+
+        List<Person> persons = new List<Person>();
+
+        for (int i = 0; i < count; i++)
+        {
+            Person person = new Person
+            {
+                CreditCardNumbers = new[] { faker.Finance.CreditCardNumber() },
+                Age = faker.Random.Int(18, 80),
+                Phones = new[] { faker.Phone.PhoneNumber() },
+                BirthDate = faker.Date.Past().ToUniversalTime().ToFileTimeUtc(),
+                Salary = faker.Random.Double(1000, 10000),
+                IsMarried = faker.Random.Bool(),
+                Gender = faker.PickRandom<Gender>(),
+                Children = GenerateRandomChildren(faker)
+            };
+
+
+            persons.Add(person);
+        }
+
+        return persons;
+    }
+
+
+    static Child[] GenerateRandomChildren(Faker faker)
+    {
+        int childCount = faker.Random.Int(0, 3);
+
+        List<Child> children = new List<Child>();
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Child child = new Child
+            {
+                Id = i + 1,
+                FirstName = faker.Name.FirstName(),
+                LastName = faker.Name.LastName(),
+                BirthDate = faker.Date.Past(10, DateTime.Now.AddYears(-18)).ToUniversalTime().ToFileTimeUtc(),
+                Gender = faker.PickRandom<Gender>()
+            };
+
+            children.Add(child);
+        }
+
+        return children.ToArray();
+    }
+}
